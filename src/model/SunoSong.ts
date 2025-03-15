61ce1aec00334a99acaabe6943eccfeeaec1b656
Pekno <m.diversay@gmail.com>
2025-03-15 21:39:05 +0100

    Fixed issue from OpenAI removal

--- src/model/SunoSong.ts
+++ src/model/SunoSong.ts
@@ -1,6 +1,3 @@
-import { z } from 'zod';
-import { zodResponseFormat } from 'openai/helpers/zod';
-
 export class SunoSong {
 	title: string;
 	lyrics: string;
@@ -8,17 +5,6 @@ export class SunoSong {
 
 	public constructor(init?: Partial<SunoSong>) {
 		Object.assign(this, init);
-	}
-
-	static getGPTObject() {
-		return zodResponseFormat(
-			z.object({
-				title: z.string(),
-				lyrics: z.string(),
-				styles: z.array(z.string()),
-			}),
-			'event'
-		);
 	}
 
 	static fromJSON(jsonData: string): SunoSong {
